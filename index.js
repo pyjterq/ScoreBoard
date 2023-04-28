fc0c29daab507d81ec89b06256e8e23844bea886
Piotr Marcinów <pyjter@protonmail.com>
2023-04-28 19:22:49 +0200

    Revert "cleaning"
    This reverts commit 538c6868d21478caa1b3bf4526a3da40902ae2cc.

--- index.js
+++ index.js
@@ -2,30 +2,16 @@ let resultHome = document.getElementById("result-home");
 let resultGuest = document.getElementById("result-guest");
 let homeResult = 0
 let guestResult = 0
-let resultBgHome = document.getElementById("result-bg-home")
-let resultBgGuest = document.getElementById("result-bg-guest")
+// let winerMark = document.querySelector(".")
 
 function plusHome(score) {
     resultHome.textContent = homeResult + parseInt(score)
     homeResult += score
-    winnerMark()
 }
 
 function plusGuest(score) { 
     resultGuest.textContent = guestResult + parseInt(score)
     guestResult += score
-    winnerMark()
-}
-
-function noMark(team) {
-    if (team === "home") {
-        resultBgHome.classList.remove("winner-bg")
-    } else if (team === "guest") {
-        resultBgGuest.classList.remove("winner-bg")
-    } else {
-        resultBgHome.classList.remove("winner-bg")
-        resultBgGuest.classList.remove("winner-bg")
-    }
 }
 
 function reset() {
@@ -33,18 +19,4 @@ function reset() {
     resultGuest.textContent = 0
     homeResult = 0
     guestResult = 0
-    noMark()
-}
-
-function winnerMark() {
-    if (homeResult > guestResult) {
-        resultBgHome.classList.add("winner-bg")
-        noMark("guest")
-    } else if (homeResult < guestResult) {
-        resultBgGuest .classList.add("winner-bg")
-        noMark("home")
-    } else {
-        noMark()
-    }
-
 }
